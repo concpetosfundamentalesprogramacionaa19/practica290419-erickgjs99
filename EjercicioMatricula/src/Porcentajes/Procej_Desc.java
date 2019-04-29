2ccfdd25af19d393d9e8ae492d1f86e641c9b180
erickgjs99 <erickjs99lol@gmail.com>
2019-04-29 16:51:16 -0500

    Ejercicio de matricula

---
+++ EjercicioMatricula/src/Porcentajes/Procej_Desc.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Porcentajes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Procej_Desc {
+        public static double matric = 1330;
+        public static double desc_prov = (matric)*0.2;
+        public static double desc_age = (matric)*0.1;
+        public static double desc_state = (matric)*0.05;
+        public static double desc_cargas = (matric)*0.03; 
+        public static double ad_admi = (matric)*0.02;
+        
+}
---
+++ EjercicioMatricula/src/ejerciciomatricula/Desarrollo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciomatricula;
+import java.util.Scanner;
+import Porcentajes.Procej_Desc;
+/**
+ *
+ * @author Usuario
+ */
+public class Desarrollo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        //Declaracion de variables
+        int city;
+        int age;
+        int state;
+        int cargas;
+        double desc_city = 0;
+        double desc_edad = 0;
+        double desc_ecv = 0;
+        double desc_cargas = 0;
+        double final_cost = 0;
+        //Desarrollo de preguntas
+        System.out.printf("Usted es de: \n "
+                + " 1._Loja\n  2._Zamora\n  3._Otras Provincias\n ");
+        city = entrada.nextInt();
+        System.out.printf("Que edad tiene? \n ");
+        age = entrada.nextInt();
+        System.out.printf("Estado civil:\n 1._Soltero\n 2._Casado\n");
+        state = entrada.nextInt();
+        System.out.printf("Tiene cargas familiares:\n 1._Si:\n 2._No \n");
+        cargas = entrada.nextInt();
+        //Condicionales para el descuento
+        if (city == 1 || city == 2){
+        desc_city =  Procej_Desc.matric - Procej_Desc.desc_prov;
+        }
+        if (age >= 17 && age < 20){
+        desc_edad = desc_city - Procej_Desc.desc_age;   
+        }
+        if (state == 2 ){
+        desc_ecv = desc_edad - Procej_Desc.desc_state;
+        }
+        if (cargas == 1){
+        desc_cargas = desc_ecv - Procej_Desc.desc_cargas;    
+        }
+        final_cost = desc_cargas + Procej_Desc.ad_admi;
+        System.out.printf("El valor total a pagar de su matricula "
+                + "es de un total de: $ %.2f", final_cost);
+    }
+}
